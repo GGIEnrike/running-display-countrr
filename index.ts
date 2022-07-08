4b660f40ef05161a3fefa885716ccb044516c8a5
GGIEnrike <enrike.churin@gmail.com>
2022-07-09 01:41:24 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 // Import stylesheets
 import './style.css';
 
-// Write Typescript code!
+let total = 31.45
+let goal = 100
+
+const totalEl = document.getElementById("total")
+const goalEl = document.getElementById("goal")
+
 const input = <HTMLInputElement>document.getElementById("input")
 input.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
