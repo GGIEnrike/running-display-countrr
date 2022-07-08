6949fd596cb366e32bafdba5bf2e942d81ec4d70
GGIEnrike <enrike.churin@gmail.com>
2022-07-09 01:47:35 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -7,10 +7,15 @@ let goal = 100
 const totalEl = document.getElementById("total")
 const goalEl = document.getElementById("goal")
 
+totalEl.innerText = total.toString()
+goalEl.innerText = goal.toString()
+
 const input = <HTMLInputElement>document.getElementById("input")
 input.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     input.blur()
+    total += input.value
     input.value = ""
+    totalEl.innerText = total.toString()
   }
 });
